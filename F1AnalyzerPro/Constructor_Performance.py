a25b2d826618489dd57f3eb8a0fe4be905bd1c34
ggmu00 <ggmu00@gmail.com>
2024-08-27 16:56:03 -0400

    Added basic function to display important constructor data, similar to that of the driver data.

--- F1AnalyzerPro/Constructor_Performance.py
+++ F1AnalyzerPro/Constructor_Performance.py
@@ -9,13 +9,14 @@ constructors = pd.read_csv('f1_data/constructors.csv')
 races = pd.read_csv('f1_data/races.csv')
 
 # Merge all important driver data together.
-def viewMajorConstructorData (driver):
+def viewMajorConstructorData (constructor):
 
     # Merges the driver_standings table and the drivers table on the driverId column
     constructor_performance = pd.merge(constructor_results, constructors, on='constructorId')
 
     # Removes columns from the table not vital to driver data
     constructor_performance.pop('url')
+
 
     # Merges the first driver performance with races table
     constructor_performance = pd.merge(constructor_performance, races, on='raceId')
@@ -24,8 +25,11 @@ def viewMajorConstructorData (driver):
     constructor_performance.pop('url')
     constructor_performance.pop('circuitId')
     constructor_performance.pop('raceId')
+    constructor_performance=constructor_performance.rename(columns = {'name_y':'raceName'})
+    constructor_performance=constructor_performance.rename(columns = {'name_x':'constructorName'})
 
-    # Gets the driver performance stats based on surname
-    performance = constructor_performance[(constructor_performance['name'] == constructors)].copy()
+
+    # Gets the driver performance stats based on constructor name
+    performance = constructor_performance[(constructor_performance['constructorName'] == constructor)].copy()
 
     print(performance.to_string())
