150eab97f230f3fc21604f5967319634f09a6cc5
ggmu00 <ggmu00@gmail.com>
2024-09-12 11:45:55 -0400

    Getting the constructor performance by year (the total amount of points at the end of each year and a points per year graph

--- F1AnalyzerPro/Constructor_Performance.py
+++ F1AnalyzerPro/Constructor_Performance.py
@@ -34,3 +34,42 @@ def viewMajorConstructorData(constructor):
     performance = constructor_performance[(constructor_performance['constructorName'] == constructor)].copy()
 
     print(performance.to_string())
+
+def constructorFinalPointsByYear(constructor):
+    # Merges the driver_standings table and the drivers table on the driverId column
+    constructor_performance = pd.merge(constructor_results, constructors, on='constructorId')
+
+    # Removes columns from the table not vital to driver data
+    constructor_performance.pop('url')
+    constructor_performance.pop('constructorResultsId')
+    constructor_performance.pop('constructorId')
+
+    # Merges the first driver performance with races table
+    constructor_performance = pd.merge(constructor_performance, races, on='raceId')
+
+    # Removes columns from the table not vital to driver data
+    constructor_performance.pop('url')
+    constructor_performance.pop('circuitId')
+    constructor_performance.pop('raceId')
+    constructor_performance = constructor_performance.rename(columns={'name_y': 'raceName'})
+    constructor_performance = constructor_performance.rename(columns={'name_x': 'constructorName'})
+
+    # Gets the driver performance stats based on constructor name
+    performance = constructor_performance[(constructor_performance['constructorName'] == constructor)].copy()
+
+    # Sort data by date
+    performance = performance.sort_values('date')
+
+    #Sum the points by year
+    result = performance.groupby('year')['points'].tail(1)
+
+    print(result.to_string())
+
+    plt.figure(figsize=(10, 5))
+    sns.lineplot(data=performance, x='year', y='position', marker='o')
+    plt.title("Constructor's Points By Year")
+    plt.gca().invert_yaxis()  # Lower number is better (1st place is better than 10th)
+    plt.xticks(performance['year'].unique())  # Ensure one-year gaps
+    plt.xlabel('Year')
+    plt.ylabel('Championship Standing Position')
+    plt.show()
--- F1AnalyzerPro/F1Analyzer.py
+++ F1AnalyzerPro/F1Analyzer.py
@@ -23,11 +23,15 @@ if selection_input == "C":
 
     #View all major constructor data throughout the years for a specified constructor
     viewMajorConstructorData(name_input)
+
+    # Get the amount of points the constructor had at the end of each year
+    constructorFinalPointsByYear(name_input)
+
 elif selection_input == "D":
     name_input = input("Enter a surname to see all driver performance data: ").capitalize()
 
     #View all major driver data throughout the years for a specified driver
     #viewMajorDriverData(name_input)
 
-    #Get the amount of points the driver had at the end of each yearyear
+    #Get the amount of points the driver had at the end of each year
     driverFinalPointsByYear(name_input)
