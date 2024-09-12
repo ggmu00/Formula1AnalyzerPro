3eae17383b2a3514a9cefb44c50c7599590e173b
ggmu00 <ggmu00@gmail.com>
2024-09-12 16:20:01 -0400

    Renamed main file, added a file that will be used to fix search errors or offer helpful feedback

---
+++ F1AnalyzerPro/SearchFixes.py
@@ -0,0 +1,10 @@
+# This file will be used to help with search bug fixes.
+# For example: User input = Lecrec
+# Feedback: "Did you mean Leclerc?"
+# Or "Here are some names that might fit the search criteria: "
+
+import pandas as pd
+from Driver_Performance import *
+from Constructor_Performance import *
+
+
