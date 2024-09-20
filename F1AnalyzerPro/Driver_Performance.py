07d2c0264468c8782b14640882a7ddbb4c152779
ggmu00 <ggmu00@gmail.com>
2024-09-19 22:16:28 -0400

    bug fix for entering the wrong name, created more bugs in while loop in the SearchData file. currently working out bug fixes

--- F1AnalyzerPro/Driver_Performance.py
+++ F1AnalyzerPro/Driver_Performance.py
@@ -8,31 +8,31 @@ driver_standings = pd.read_csv('f1_data/driver_standings.csv')
 drivers = pd.read_csv('f1_data/drivers.csv')
 races = pd.read_csv('f1_data/races.csv')
 
+# Merges the driver_standings table and the drivers table on the driverId column
+driver_performance = pd.merge(driver_standings, drivers, on='driverId')
+
+# Removes columns from the table not vital to driver data
+driver_performance.pop('url')
+driver_performance.pop('driverStandingsId')
+driver_performance.pop('driverId')
+driver_performance.pop('dob')
+driver_performance.pop('driverRef')
+
+# Merges the first driver performance with races table
+driver_performance = pd.merge(driver_performance, races, on='raceId')
+
+# Removes columns from the table not vital to driver data
+driver_performance.pop('url')
+driver_performance.pop('circuitId')
+driver_performance.pop('raceId')
+
+
 def getSpecificDriver(driver):
-    # Merges the driver_standings table and the drivers table on the driverId column
-    driver_performance = pd.merge(driver_standings, drivers, on='driverId')
-
-    # Removes columns from the table not vital to driver data
-    driver_performance.pop('url')
-    driver_performance.pop('driverStandingsId')
-    driver_performance.pop('driverId')
-    driver_performance.pop('dob')
-    driver_performance.pop('driverRef')
-
-    # Merges the first driver performance with races table
-    driver_performance = pd.merge(driver_performance, races, on='raceId')
-
-    # Removes columns from the table not vital to driver data
-    driver_performance.pop('url')
-    driver_performance.pop('circuitId')
-    driver_performance.pop('raceId')
-
     # Gets the driver performance stats based on surname
     performance = driver_performance[(driver_performance['surname'] == driver)].copy()
 
     # Sorts the dataset by the forename
     performance = performance.sort_values('forename')
-
 
     # Compares each name to the previous name to see if it is equal. If it is not
     # equal it returns false.
@@ -42,13 +42,11 @@ def getSpecificDriver(driver):
     # surname but different forename
     filtered_df = performance[performance['matchName'] == False]
 
-
     # Gets the number of names by getting the amount of rows
     number_of_names = len(filtered_df)
 
     # Prompting the user for input and storing it in a variable
     if number_of_names > 1:
-
         # Print drivers with same surname
         print(filtered_df[['forename']])
 
@@ -60,9 +58,9 @@ def getSpecificDriver(driver):
 
     return performance
 
-# Merge all important driver data together.
-def viewMajorDriverData (driver):
 
+# Merge all important driver data together.
+def viewMajorDriverData(driver):
     # Call the function to get specific driver data
     performance = getSpecificDriver(driver)
 
@@ -78,8 +76,8 @@ def viewMajorDriverData (driver):
     plt.ylabel('Championship Standing Position')
     plt.show()
 
-def driverFinalPointsByYear(driver):
 
+def driverFinalPointsByYear(driver):
     # Call the function to get specific driver data
     performance = getSpecificDriver(driver)
 
@@ -102,3 +100,66 @@ def driverFinalPointsByYear(driver):
     plt.xlabel('Year')
     plt.ylabel('Championship Standing Position')
     plt.show()
+
+
+def firstThreeLettersMatch(driver):
+    # Setting the driver variable to the first three characters of the name
+    driver = driver[:3]
+
+    first_3_check = driver_performance
+    first_3_check['f3c'] = driver_performance['surname'].str.contains(driver)
+    last_3_filtered = first_3_check[first_3_check['f3c']]
+
+    # Sorts the dataset by the forename
+    performance = last_3_filtered.sort_values('forename')
+
+    # Compares each name to the previous name to see if it is equal. If it is not
+    # equal it returns false.
+    performance['matchName'] = performance.forename.eq(performance.forename.shift())
+
+    # Dataframe is created to only hold the data of the names of drivers that have same
+    # surname but different forename
+    filtered_df = performance[performance['matchName'] == False]
+
+    # Compares each name to the previous name to see if it is equal. If it is not
+    # equal it returns false.
+    performance['matchName2'] = performance.surname.eq(performance.surname.shift())
+
+    # Dataframe is created to only hold the data of the names of drivers that have same
+    # surname but different forename
+    filtered_df = performance[performance['matchName2'] == False]
+
+    # Print drivers with same surname
+    print(filtered_df[['surname']])
+
+
+def lastThreeLettersMatch(driver):
+    # Setting the driver variable to the last three characters of the name
+    driver = driver[-3:]
+
+    last_3_check = driver_performance
+    last_3_check['f3c'] = driver_performance['surname'].str.contains(driver)
+    last_3_filtered = last_3_check[last_3_check['f3c']]
+
+    # Sorts the dataset by the forename
+    performance = last_3_filtered.sort_values('forename')
+
+    # Compares each name to the previous name to see if it is equal. If it is not
+    # equal it returns false.
+    performance['matchName'] = performance.forename.eq(performance.forename.shift())
+
+    # Dataframe is created to only hold the data of the names of drivers that have same
+    # surname but different forename
+    filtered_df = performance[performance['matchName'] == False]
+
+    # Compares each name to the previous name to see if it is equal. If it is not
+    # equal it returns false.
+    performance['matchName2'] = performance.surname.eq(performance.surname.shift())
+
+    # Dataframe is created to only hold the data of the names of drivers that have same
+    # surname but different forename
+    filtered_df = performance[performance['matchName2'] == False]
+
+    # Print drivers with same surname
+    print(filtered_df[['surname']])
+
--- F1AnalyzerPro/SearchData.py
+++ F1AnalyzerPro/SearchData.py
@@ -26,40 +26,65 @@ def userInput():
         #View all major driver data throughout the years for a specified driver
         #viewMajorDriverData(name_input)
 
-        driverNameCheck(name_input)
+
+
+        # This sets the driver data to the function getFinalPointsByYear;
+        # in the future change to pick the data the user wants to see
+
+        check3 = ""
+
+        while check3 == "":
+            if name_input not in getSpecificDriver(name_input):
+                firstThreeLettersMatch(name_input)
+                check = input("Do you see the name you were meaning?(Y/N)").capitalize()
+                if check == "Y":
+                    name_input = input("Please enter the name exactly as you see in the above list:")
+                    check3 = name_input
+                    driverFinalPointsByYear(name_input)
+                elif check == "N":
+                    lastThreeLettersMatch(name_input)
+                    check2 = input("Do you see the name you were meaning?(Y/N)")
+                    if check2 == "Y":
+                        name_input = input("Please enter the name exactly as you see in the above list:")
+                        check3 = name_input
+                        driverFinalPointsByYear(name_input)
+                    elif check2 == "N":
+                        check3 = input("Please try again")
+
+        #firstThreeLettersMatch(name_input)
+        #getSpecificDriver(lastThreeLettersMatch(name_input))
 
         #Get the amount of points the driver had at the end of each year
-        driverFinalPointsByYear(name_input)
+        #driverFinalPointsByYear(name_input)
+
 
 def driverNameCheck(driver):
-    #name_check = getSpecificDriver(driver)
+    """ driver_df = getSpecificDriver(driver)
     name_input = ""
-    count =0
+    print("tahdah")
+    first_3_letters_check = driver_df[driver_df["surname"].str.contains(driver[:3])]
 
-    while count == 0:
-        if driver in getSpecificDriver(driver).values:
-            count = count + 1
-            return driver
-        elif driver[:3] in getSpecificDriver(driver).values[:3]:
-            temp_df = getSpecificDriver(driver)[(getSpecificConstructor(driver)['surname'] == driver[:3])].copy()
-            temp_df = temp_df['matchName'] = temp_df.forename.eq(temp_df.forename.shift())
-            filtered_df = temp_df[temp_df['matchName']==True]
-            print(filtered_df)
-            name_input = input("Select your driver from the list above: ")
-            count = count + 1
-            return name_input
-        elif driver[-3:] in getSpecificDriver(driver).values[-3:]:
-            temp_df = getSpecificDriver(driver)[(getSpecificConstructor(driver)['surname'] == driver[-3:])].copy()
-            temp_df = temp_df['matchName'] = temp_df.forename.eq(temp_df.forename.shift())
-            filtered_df = temp_df[temp_df['matchName']==True]
-            print(filtered_df)
-            name_input = input("Select your driver from the list above: ")
-            count = count + 1
-            return name_input
-        else:
-            name_input = input("Please input the name again: ")
+    print(first_3_letters_check)
 
+    if driver in getSpecificDriver(driver).values:
+        print("work")
+        return driver
+    elif driver[:3] in first_3_letters_check.values:
+        print(first_3_letters_check)
+        #getSpecificDriver(driver)
+        # getSpecificDriver(driver_first_3)
+        # print(name_check)
+        #name_input = input("Select your driver from the list above: ")
+        #return name_input
+    elif driver[-3:] in getSpecificDriver(driver).values[-3:]:
+        temp_df = getSpecificDriver(driver)[(getSpecificConstructor(driver)['surname'] == driver[-3:])].copy()
+        temp_df = temp_df['matchName'] = temp_df.forename.eq(temp_df.forename.shift())
+        filtered_df = temp_df[temp_df['matchName']==True]
+        print(filtered_df)
+        name_input = input("Select your driver from the list above: ")
+        return name_input
+    else:
+        name_input = input("Please input the name again: ")
+        return name_input
 
-
-
-
+        """
